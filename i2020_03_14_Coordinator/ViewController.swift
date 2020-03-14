3bb5cdb6d8e6ca48f298a08e82ce2df702f94c43
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2020-03-14 19:03:38 -0400

    Initial Commit

---
+++ i2020_03_14_Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i2020_03_14_Coordinator
+//
+//  Created by Bradford, Phillip on 3/14/20.
+//  Copyright © 2020 Bradford, Phillip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i2020_03_14_CoordinatorTests/i2020_03_14_CoordinatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  i2020_03_14_CoordinatorTests.swift
+//  i2020_03_14_CoordinatorTests
+//
+//  Created by Bradford, Phillip on 3/14/20.
+//  Copyright © 2020 Bradford, Phillip. All rights reserved.
+//
+
+import XCTest
+@testable import i2020_03_14_Coordinator
+
+class i2020_03_14_CoordinatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
